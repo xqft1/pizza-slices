55c045336478c2fee1885ca5d412f8971dea5847
xQft1 <robert.s.towner@gmail.com>
2026-07-22 23:28:41 +0100

    Update server.js

--- metadata/server.js
+++ metadata/server.js
@@ -4,6 +4,8 @@ import { Contract } from "ethers";
 import { provider } from "./provider.js";
 import { ABI } from "./abi.js";
 import { LEVELS } from "./levels.js";
+import fs from "fs";
+import path from "path";
 
 const app = express();
 
@@ -13,6 +15,7 @@ const contract = new Contract(
   provider
 );
 
+// Metadata endpoint
 app.get("/metadata/:id", async (req, res) => {
   try {
     const tokenId = req.params.id;
@@ -23,7 +26,7 @@ app.get("/metadata/:id", async (req, res) => {
     res.json({
       name: `Pizza Slice #${String(tokenId).padStart(5, "0")}`,
       description: "Dynamic PizzaSlices NFT",
-      image: `https://satopizza.xyz/${slice.image}`,
+      image: `https://metadata.satopizza.xyz/image/${tokenId}`,
       attributes: [
         {
           trait_type: "Slice",
@@ -37,6 +40,33 @@ app.get("/metadata/:id", async (req, res) => {
     });
   } catch (err) {
     res.status(500).json({ error: err.message });
+  }
+});
+
+// Dynamic SVG image endpoint
+app.get("/image/:id", async (req, res) => {
+  try {
+    const tokenId = req.params.id;
+
+    const level = Number(await contract.sliceLevel(tokenId));
+    const slice = LEVELS[level] ?? LEVELS[0];
+
+    const svgPath = path.join(
+      process.cwd(),
+      "assets",
+      `${slice.name}.svg`
+    );
+
+    let svg = fs.readFileSync(svgPath, "utf8");
+
+    svg = svg
+      .replace("{{IMAGE}}", `https://satopizza.xyz/${slice.image}`)
+      .replace("{{TOKEN}}", String(tokenId).padStart(5, "0"));
+
+    res.setHeader("Content-Type", "image/svg+xml");
+    res.send(svg);
+  } catch (err) {
+    res.status(500).send(err.message);
   }
 });
 
