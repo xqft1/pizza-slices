0c100987ca53a731d3ccdffee95aa04bf8a52f2d
xQft1 <robert.s.towner@gmail.com>
2026-07-22 21:46:21 +0100

    Create levels.js

---
+++ metadata/levels.js
@@ -0,0 +1,38 @@
+export const LEVELS = {
+  0: {
+    name: "CHEESE.EXE",
+    image: "CHEESE.png"
+  },
+  1: {
+    name: "PEPPERONI.EXE",
+    image: "PEPPERONI.png"
+  },
+  2: {
+    name: "HAWAIIAN.EXE",
+    image: "HAWAIIAN.png"
+  },
+  3: {
+    name: "MEATLOVR.EXE",
+    image: "MEATLOVR.png"
+  },
+  4: {
+    name: "VEGGIE.EXE",
+    image: "VEGGIE.png"
+  },
+  5: {
+    name: "SUPREME.EXE",
+    image: "SUPREME.png"
+  },
+  6: {
+    name: "VOLCANO.EXE",
+    image: "VOLCANO.png"
+  },
+  7: {
+    name: "BITCOIN.EXE",
+    image: "BITCOIN.png"
+  },
+  8: {
+    name: "GOLDEN.EXE",
+    image: "GOLDEN.png"
+  }
+};
