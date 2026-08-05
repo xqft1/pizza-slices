5093f87dfb7c8eb91c9c6b85cb17bf75172e65db
xQft1 <robert.s.towner@gmail.com>
2026-08-05 23:40:12 +0100

    Update server.js

--- metadata/server.js
+++ metadata/server.js
@@ -32,6 +32,9 @@ app.get("/metadata/:id", async (req, res) => {
     const level = Number(await contract.sliceLevel(tokenId));
     const slice = LEVELS[level] ?? LEVELS[0];
 
+    const crustLevel = Number(await crustOven.crustLevel(tokenId));
+    const crust = CRUSTS[crustLevel] ?? CRUSTS[0];
+
     res.json({
       name: `Pizza Slice #${String(tokenId).padStart(5, "0")}`,
       description: "Dynamic PizzaSlices NFT",
