a3bbdd37158016d65ed4420bbe9c884754d3b9d0
xQft1 <robert.s.towner@gmail.com>
2026-08-05 23:36:12 +0100

    Create crusts.js

---
+++ metadata/crusts.js
@@ -0,0 +1,29 @@
+export const CRUSTS = [
+  {
+    name: "PLAIN"
+  },
+  {
+    name: "THIN"
+  },
+  {
+    name: "DEEP PAN"
+  },
+  {
+    name: "STUFFED"
+  },
+  {
+    name: "CHEESE STUFFED"
+  },
+  {
+    name: "HOT DOG STUFFED"
+  },
+  {
+    name: "GARLIC STUFFED"
+  },
+  {
+    name: "GOLDEN"
+  },
+  {
+    name: "IMMORTAL"
+  }
+];
